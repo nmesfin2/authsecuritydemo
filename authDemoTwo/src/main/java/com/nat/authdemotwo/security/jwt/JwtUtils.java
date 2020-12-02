d4ccf9c0d8fa5f65cb9c70d98b5792bf87dd112c
nat <nmexodus870@gmail.com>
2020-12-02 15:42:08 +0000

    # WARNING: head commit changed in the meantime
    JwtUtils

--- authDemoTwo/src/main/java/com/nat/authdemotwo/security/jwt/JwtUtils.java
+++ authDemoTwo/src/main/java/com/nat/authdemotwo/security/jwt/JwtUtils.java
@@ -1,14 +1,35 @@
 package com.nat.authdemotwo.security.jwt;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 
+import com.nat.authdemotwo.security.services.UserDetailsImpl;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+
 public class JwtUtils {
+	
+	@Value("authdemo.app.jwtSecret")
+	private String jwtSecret;
+	
+	@Value("authdemo.app.jwtExpirationMs")
+	private int jwtExpirationMs;
 	public String generateJwtToken(Authentication authentication) {
 		// userName
 		
 		// authenticated username and password will be provided by 
-		authentication.getPrincipal();
+		UserDetailsImpl userDetailsImpl =(UserDetailsImpl)authentication.getPrincipal();
 		// we need to build token
+		return Jwts.builder().setSubject(userDetailsImpl.getUsername())
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(new Date().getTime()+jwtExpirationMs))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.compact();
 		// we need current time to provide 
 		// we need expiration time.
 		// a log for generationg token
@@ -16,10 +37,17 @@ public class JwtUtils {
 	}
 	
 	public String getUserNameFromJwtToken(String token) {
-		
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
 	}
 	
 	public boolean validateJwtToken(String authToken) {
-		
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+			return true;
+		}catch(SignatureException e){
+			return false;
+		}catch(MalformedJwtException e) {
+			return false;
+		}
 	}
 }
