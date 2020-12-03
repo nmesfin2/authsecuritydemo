8dde553bb1806f8f774c8d22b669baaa1b8201ea
nat <nmexodus870@gmail.com>
2020-12-03 16:20:44 +0000

    security part 1

---
+++ authDemoTwo/src/main/java/com/nat/authdemotwo/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,30 @@
+package com.nat.authdemotwo.security.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.nat.authdemotwo.security.services.UserDetailsServiceImpl;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+	@Autowired
+	JwtUtils jwtUtils;
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ authDemoTwo/src/main/java/com/nat/authdemotwo/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.nat.authdemotwo.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nat.authdemotwo.model.User;
+import com.nat.authdemotwo.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with username " + username));
+		return UserDetailsImpl.build(user);
+	}
+
+}
